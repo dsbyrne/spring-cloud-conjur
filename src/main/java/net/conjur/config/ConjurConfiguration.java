37db80188813503d99d9a1e18fe96a54b9b2bbd1
Dustin Byrne <dbyrne@conjur.net>
2017-10-03 13:31:29 -0400

    remove extra dependencies

--- src/main/java/net/conjur/config/ConjurConfiguration.java
+++ src/main/java/net/conjur/config/ConjurConfiguration.java
@@ -1,13 +1,7 @@
 package net.conjur.config;
 
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.core.annotation.Order;
-import org.springframework.core.Ordered;
-
-import net.conjur.api.Conjur;
 
 @Configuration
 public class ConjurConfiguration {
